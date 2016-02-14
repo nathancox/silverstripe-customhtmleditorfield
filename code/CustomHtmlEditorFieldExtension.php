f8adc7648fa943c5d5cf649e5e2c456e9dfc1e08
spekulatius <petert@catalyst.net.nz>
2016-02-14 20:00:31 +1300

    Remove trailing spaces in the source code

--- code/CustomHtmlEditorFieldExtension.php
+++ code/CustomHtmlEditorFieldExtension.php
@@ -1,55 +1,55 @@
 <?php
 /**
  *	An extension of HTMLEditorField that lets you set editor-specific body class names and TinyMCE configs
- *	
+ *
  */
 class CustomHtmlEditorFieldExtension extends Extension {
-	
-	
+
+
 	/**
-	 * 	
+	 *
 	 *	@param string $identifier
 	 */
 	function setEditorConfig($identifier) {
 		$this->owner->setAttribute('data-config-id', $identifier);
 	}
-	
+
 	/**
 	 * 	Get the identifier for this field's HTMLEditorConfig
-	 * 	
+	 *
 	 *	@return string
 	 */
 	function getEditorConfigID() {
 		return $this->owner->getAttribute('data-config-id');
 	}
-	
+
 	/**
 	 * 	Return this field's HTMLEditorConfig
-	 * 	
+	 *
 	 *	@return HTMLEditorConfig
 	 */
 	function getEditorConfig() {
 		return HTMLEditorConfig::get($this->getEditorConfigID());
 	}
-	
-	
+
+
 	/**
 	 * 	Convenience method for setting body classes.  Just slightly nicer than using setAttribute().
 	 * 	NOTE: the specified classes will be APPENDED to the config's body_class property, not replace it
-	 * 	
+	 *
 	 *	@param string $classes
 	 */
 	function setBodyClass($classes) {
 		$this->owner->setAttribute('data-body-class', $classes);
 	}
-	
+
 	/**
 	 * 	Return This field's body class setting
-	 * 	
+	 *
 	 *	@return string
 	 */
 	function getBodyClass() {
 		$this->owner->getAttribute('data-body-class');
 	}
-	
+
 }
--- javascript/CustomHTMLEditorField.js
+++ javascript/CustomHTMLEditorField.js
@@ -1,26 +1,26 @@
 /**
  *	Overwrites the TinyMCE instantiation on HTMLEditorFields.  Does two things:
- *	
- *	1) allows you to set custom body classes for each editor.  This is done via the data-body-class attribute 
+ *
+ *	1) allows you to set custom body classes for each editor.  This is done via the data-body-class attribute
  *	or by setBodyClass() on HTMLEditorField.  The body class is appended to the config's default.
- *	2) allow for per-editor TinyMCE configs setting the data-config-id attribute (done with setEdtorConfig on 
+ *	2) allow for per-editor TinyMCE configs setting the data-config-id attribute (done with setEdtorConfig on
  *	HTMLEditoField)
  */
 (function($) {
 	$.entwine('ss', function($) {
 		$('textarea.htmleditor').entwine({
-			
+
 			redraw: function() {
 
 				var config, self = this, ed = this.getEditor();
-				
-				// if this editor has the data-config-id attribute set and customTinyMceConfigs is defined, use them to 
+
+				// if this editor has the data-config-id attribute set and customTinyMceConfigs is defined, use them to
 				// get the config.
 				// otherwise use the default in ssTinyMceConfigs
 				if ($(this).data('config-id') && typeof customTinyMceConfigs == 'object') {
 					config = customTinyMceConfigs[$(this).data('config-id')];
-				} 
-				
+				}
+
 				if (!config) {
 					config = ssTinyMceConfig;
 				}
@@ -41,21 +41,21 @@
 					'setupcontent_callback',
 					'urlconverter_callback'
 				];
-				
+
 				var i = 0;
 				var property;
-				
+
 				for (i = 0; i < callbackProperties.length; i++) {
 					property = callbackProperties[i];
-					
+
 					if (typeof config[property] != 'undefined') {
 						config.property = function() { eval(config[property]) };
 					}
-					
+
 				}
-				
+
 				var customConfig = {};
-				
+
 				// if a custom body class was defined, append it to the default and merge it with the config
 				if ($(this).data('body-class')) {
 					if (!config.body_class) {
@@ -86,10 +86,9 @@
 						redrawObj.show();
 					}, 10);
 				});
-		
+
 			}
-			
+
 		});
 	});
 })(jQuery);
-	
